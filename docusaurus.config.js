6140fffac9510dd041ca5be4ffbdbba632f64ee5
kulchitskyi <kulchitskiy.666@gmail.com>
2024-05-16 01:39:44 +0300

    Push 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'kulchitskyifacebook', // Usually your GitHub org/user name.
+  organizationName: 'kulchitskyi', // Usually your GitHub org/user name.
   projectName: 'Lab2', // Usually your repo name.
 
   onBrokenLinks: 'throw',
