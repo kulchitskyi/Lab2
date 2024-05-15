d576213733ea1cfa071267c50646eebe1772dc5d
kulchitskyi <kulchitskiy.666@gmail.com>
2024-05-16 01:38:00 +0300

    Changed site name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://kulchitskyi.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Lab2/',
+  baseUrl: '/lab2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
